86004401621d1d63afca93266dc17b74c9e81813
Wang Meiying <52064747+calfoy@users.noreply.github.com>
2025-04-07 21:22:40 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import {Wheel} from '../../../dist/spin-wheel-esm.js';
+import {Wheel} from '../dist/spin-wheel-esm.js';
 import {loadFonts, loadImages} from '../../../scripts/util.js';
 import {props} from './props.js';
 
